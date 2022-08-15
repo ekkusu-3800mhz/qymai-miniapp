7c8f97ceac54ae772912db5bf0c2b36475df5ee5
Shinra DX <shinra.dx@outlook.com>
2022-08-15 11:21:53 +0800

    设定全局定时器，隔30秒获取一次用户位置

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 App({
     onLaunch() {
         wx.cloud.init();
+        let location = wx.getLocation({type: 'gcj02'});
+        location.then((res) => {
+            this.globalData.userLocation = {
+                longitude: res.longitude,
+                latitude: res.latitude,
+            };
+        });
+        let interval = setInterval(() => {
+            let location = wx.getLocation({type: 'gcj02'});
+            location.then((res) => {
+                this.globalData.userLocation = {
+                    longitude: res.longitude,
+                    latitude: res.latitude,
+                };
+            });
+        }, 30000);
     },
+
+    globalData: {
+        userLocation: {
+            longitude: 0,
+            latitude: 0,
+        },
+    },
+
 })
