749f406eea7ad333f6340ab2ddfd73f575b7c155
Shinra DX <shinra.dx@outlook.com>
2022-08-15 11:23:01 +0800

    封装腾讯地图JS-SDK距离计算函数

--- utils/functions.js
+++ utils/functions.js
@@ -1,7 +1,5 @@
-const WX_CLOUD_CONF = {
-    ENV: "",
-    SERVICE: "",
-};
+const QQMapWX = require("../libs/qqmap-wx-jssdk.js");
+const { WX_CLOUD_CONF, QQ_MAP_CONF } = require("../app.config.js");
 
 const callContainer = (path, method, data = null, extHeader = null) => {
     if (extHeader) {
@@ -21,8 +19,30 @@ const callContainer = (path, method, data = null, extHeader = null) => {
         method,
         data,
     });
-}
+};
+
+const callQQMap = () => {
+    return new QQMapWX({
+        key: QQ_MAP_CONF.KEY,
+    });
+};
+
+const calculateDistance = (from, to) => {
+    let map = callQQMap();
+    return new Promise((resolve, reject) => {
+        map.calculateDistance({
+            mode: 'straight',
+            from,
+            to,
+            success(res) {
+                resolve(res.result.elements[0].distance);
+            },
+        });
+    });
+};
 
 module.exports = {
     callContainer,
+    callQQMap,
+    calculateDistance,
 };
